9c57c3e43deca2e6c68568e091050d9646f96399
Ibrahim Mohamed <120879381+solitaryibra@users.noreply.github.com>
2025-05-07 17:28:53 +0200

    Update contactme.php

--- contactme.php
+++ contactme.php
@@ -9,8 +9,14 @@
     $body = "<ul><li>Name: ".$name."</li><li>Phone: ".$phone."</li><li>Email: ".$email."</li><li>Message: ".$message."</li></ul>";
 
     $status = mailfunction("solitaryibra@gmail.com", "Company", $body); //reciever
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Handle form submission here
+} else {
+    echo 'Request method not allowed';
+}
     if($status)
         echo '<center><h1>Thanks! We will contact you soon.</h1></center>';
+        
     else
         echo '<center><h1>Error sending message! Please try again.</h1></center>';    
 ?>
