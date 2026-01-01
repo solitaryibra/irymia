0b48f2b0e97e4f02277745ffdb8bc5732579f8f3
Ibrahim Mohamed <120879381+solitaryibra@users.noreply.github.com>
2026-01-01 23:25:00 +0100

    Create people.js with details of individuals
    Added a list of people with their details.

---
+++ src/data/people.js
@@ -0,0 +1,11 @@
+export const people = [
+  {
+    name: "Your Name",
+    role: "Biomedical Engineer",
+    interests: "Biomechanics, Motion Analysis, Rehabilitation",
+    links: {
+      linkedin: "",
+      github: ""
+    }
+  }
+];
